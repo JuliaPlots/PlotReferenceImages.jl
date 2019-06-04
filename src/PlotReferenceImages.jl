7c5e67dd425d69132183e049d4f9e7946ceaa790
Daniel Schwabeneder <daschw@disroot.org>
2019-06-04 22:43:18 +0200

    initial rewrite

--- src/PlotReferenceImages.jl
+++ src/PlotReferenceImages.jl
@@ -1,14 +1,19 @@
 module PlotReferenceImages
 # A module is required for `pathof(PlotReferenceImages)` to work.
 
-using Requires
+using DataStructures
+using StatsPlots, RDatasets, ProgressMeter, DataFrames, Distributions, StatsBase
 
+# import and initialize plotting backends
+import PyPlot, PlotlyJS, ORCA, PGFPlots
+PyPlot.ioff()
 
 # ------------------------------------------------------------------------------
 # Plots docs and tests reference images
+theme(:default)
 
-function __init__()
-    @require Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("plotdocs.jl")
-end
+include("plotdocs.jl")
+
+export generate_doc_image, generate_doc_images, generate_reference_image, generate_reference_images
 
 end # module
