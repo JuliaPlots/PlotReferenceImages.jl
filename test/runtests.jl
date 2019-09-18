114578802e3e8a485d45ac32ff1cd582d19c2e75
Daniel Schwabeneder <daschw@disroot.org>
2019-09-18 19:14:40 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test, PlotReferenceImages
 
 @testset "PlotReferenceImages" begin
-    @test isdir(reference_path(:notabackend, v"1"))
-    @test isfile(reference_file(:gr, v"1"))
+    @test !isdir(reference_path(:notabackend, v"1"))
+    @test isfile(reference_file(:gr, 1, v"1"))
 end
